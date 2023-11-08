b0a9be5cf384344061e799a6fc783ba244f0af93
Felipe Miotto <felipe_miotto@hotmail.com>
2023-11-08 19:48:29 -0300

    feat: change bio name

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function About() {
-  return (
-    <main>
-      <div>about</div>
-    </main>
-  )
-}
---
+++ app/bio/page.tsx
@@ -0,0 +1,11 @@
+export default function Bio() {
+  return (
+    <main className="page-content">
+      <h1>Biografy</h1>
+      <p>asd asd  d asdf ASDs adSDAsda ASD asdA Dsasd ASD asdA SD asd</p>
+      <p>asd asd  d asdf ASDs adSDAsda ASD asdA Dsasd ASD asdA SD asd</p>
+      <p>asd asd  d asdf ASDs adSDAsda ASD asdA Dsasd ASD asdA SD asd</p>
+      <p>asd asd  d asdf ASDs adSDAsda ASD asdA Dsasd ASD asdA SD asd</p>
+    </main>
+  )
+}
