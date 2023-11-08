0a8cac3f9ecd2644c16865539a3ab9e349056a3f
Felipe Miotto <felipe_miotto@hotmail.com>
2023-11-08 19:50:52 -0300

    feat: add home content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Link from 'next/link'
+import SocialMedia from "@/components/social-media/SocialMedia";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <div>oiww</div>
+    <main className="page-content">
+        <h1 className="text-1xl sm:text-1xl md:text-2xl lg:text-4xl"><b>You&apos;re Welcome</b>, Felipe here! I&apos;m a guy passionate about technology =)</h1>
+        <h2 className="px-[12vw] mt-5">I&apos;m a fullstack web developer lorem ipsum setasdasd  asdasda aspda I&apos;m a fullstack</h2>
+        <div className="flex justify-between mt-20 px-[5vw] md:px-[10vw] mb-20" >
+          <Link href={'/bio'}>More about me</Link>
+          <Link href={'/experiences'}>See my experiences</Link>
+        </div>
+        <SocialMedia />
     </main>
   )
 }
