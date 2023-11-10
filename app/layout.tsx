5d04e5320fef8af08af4139894d9f2a8351068c5
Felipe Miotto <felipe_miotto@hotmail.com>
2023-11-10 14:57:32 -0300

    feat: add theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Navbar from '@/components/navbar/Navbar'
+import { Providers } from '@/components/theme-switcher/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   )
