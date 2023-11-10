c3b3c5fb900657cf42e389060be5f97c8fffb522
Felipe Miotto <felipe_miotto@hotmail.com>
2023-11-10 15:16:46 -0300

    fix: adjust header menu colors

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -10,39 +10,37 @@ import { useTheme } from 'next-themes';
 type HeaderProps = {
   navbarOpen: boolean
   setNavbarOpen: (isOpen: boolean) => void,
-  isDarkMode: boolean
 }
 
-const Header = ({ navbarOpen, setNavbarOpen, isDarkMode }: HeaderProps) => {
+const Header = ({ navbarOpen, setNavbarOpen }: HeaderProps) => {
   // const [isDarkMode, setIsDarkMode] = useState<boolean>(true)
-  const { theme, setTheme } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className='flex flex-row justify-between px-[8%] py-5 sm:py-8 lg:py-10 text-center'>
       <Link className='self-center text-xl' href="/"><b>Felipe M.</b> </Link>
       <div className='flex flex-row justify-between gap-x-5 items-center'>
-      theme{theme}
           <LocaleSwitcher />
 
           <ThemeSwitcher />
           {/* <div className='cursor-pointer' onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode ? <BsFillMoonFill /> : <BsFillSunFill />}</div> */}
           <button
-          className={`flex top-0 right-0 z-20 relative w-10 h-10 text-${theme === 'light' ? 'black' : 'white'} focus:outline-none`}
+          className={`flex top-0 right-0 z-20 relative w-10 h-10 text-${theme === 'dark' ? 'white' : 'black'} focus:outline-none`}
           onClick={() => setNavbarOpen(!navbarOpen)}
           >
             <div className="absolute w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
                 <span
-                  className={`absolute h-0.5 w-5 bg-${theme === 'light' ? 'black' : 'white'} transform transition duration-300 ease-in-out ${
+                  className={`absolute h-0.5 w-5 bg-${theme === 'dark' ? 'white' : 'black'} transform transition duration-300 ease-in-out ${
                       navbarOpen ? "rotate-45 delay-200" : "-translate-y-1.5"
                   }`}
                 ></span>
                 <span
-                  className={`absolute h-0.5 bg-${theme === 'light' ? 'black' : 'white'} transform transition-all duration-200 ease-in-out ${
+                  className={`absolute h-0.5 bg-w ${theme === 'dark' ? 'bg-white' : 'bg-black'} transform transition-all duration-200 ease-in-out ${
                       navbarOpen ? "w-0 opacity-50" : "w-5 delay-200 opacity-100"
                   }`}
                 ></span>
                 <span
-                  className={`absolute h-0.5 w-5 bg-${theme === 'light' ? 'black' : 'white'} transform transition duration-300 ease-in-out ${
+                  className={`absolute h-0.5 w-5 bg-${theme === 'dark' ? 'white' : 'black'} transform transition duration-300 ease-in-out ${
                       navbarOpen ? "-rotate-45 delay-200" : "translate-y-1.5"
                   }`}
                 ></span>
