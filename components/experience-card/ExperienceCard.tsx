11c2b30f4ccacf4ca707fd32472186a4cd8f16dd
Felipe Miotto <felipe_miotto@hotmail.com>
2023-11-30 17:40:16 -0300

    feat: add experience card component

---
+++ components/experience-card/ExperienceCard.tsx
@@ -0,0 +1,25 @@
+type ExperienceCardProps = {
+    title: string;
+    company: string;
+    date: string;
+    description: string[];
+}
+
+function ExperienceCard({ title, company, date, description }: ExperienceCardProps) {
+  return (
+    <div className="border-b-2 pb-4 pt-4">
+      <div className="flex justify-between flex-col md:flex-row items-center mb-4">
+        <b>{company}</b>
+        <div className="flex flex-col mt-7">
+          <p>{title}</p>
+          <p>{date}</p>
+        </div>
+      </div>
+      <ul>
+        {description.map((desc) => <li>{desc}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default ExperienceCard
