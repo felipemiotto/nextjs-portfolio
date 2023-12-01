547437666db2f96b5a71c4f5b08ac935b896b776
Felipe Miotto <felipe_miotto@hotmail.com>
2023-12-01 16:33:29 -0300

    feat: add content to bio page

--- app/[lang]/bio/page.tsx
+++ app/[lang]/bio/page.tsx
@@ -1,6 +1,8 @@
 import { getDictionary } from '@/lib/get-dictionary'
 import  { Locale } from '@/i18n-config'
 import PageTitle from '@/components/page-title/PageTitle'
+import Image from 'next/image'
+import bioImage from '@/public/bio.jpg';
 
 export default async function Bio(
   { params: {lang}}: { params: { lang: Locale }}
@@ -8,9 +10,18 @@ export default async function Bio(
   const dictionary = await getDictionary(lang)
 
   return (
-    <main className="page-content">
+    <main className="page-content mb-20">
       <PageTitle title={dictionary.bio.title}/>
-      <p>{dictionary.bio.text}</p>
+      <div className='flex flex-col gap-10'>
+        <p>{dictionary.bio.text}</p>
+        <div className='md:px-10 lg:px-40 xl:px-52'>
+          <Image
+            objectFit='contain'
+            src={bioImage}
+            alt="Felipe Antonio Miotto with its son" 
+          />
+        </div>
+      </div>
     </main>
   )
 }
