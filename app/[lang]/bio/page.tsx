86993d781b183ac4a68fa677c263f57d553581b8
Felipe Miotto <felipe_miotto@hotmail.com>
2023-12-04 10:15:26 -0300

    feat: add resume link to bio page WIP

--- app/[lang]/bio/page.tsx
+++ app/[lang]/bio/page.tsx
@@ -3,6 +3,9 @@ import  { Locale } from '@/i18n-config'
 import PageTitle from '@/components/page-title/PageTitle'
 import Image from 'next/image'
 import bioImage from '@/public/bio.jpg';
+import cv_pt from '@/public/cv_pt.pdf';
+import cv_en from '@/public/cv_en.pdf';
+import Link from 'next/link';
 
 export default async function Bio(
   { params: {lang}}: { params: { lang: Locale }}
@@ -16,11 +19,21 @@ export default async function Bio(
         <p>{dictionary.bio.text}</p>
         <div className='md:px-10 lg:px-40 xl:px-52'>
           <Image
-            objectFit='contain'
             src={bioImage}
             alt="Felipe Antonio Miotto with its son" 
           />
         </div>
+      </div>
+      <div className='mt-10 text-center'>
+        <Link 
+          target='_blank' 
+          href={dictionary['current-locale'] === 'pt' ? '/cv_pt.pdf' : '/cv_en.pdf'}
+          rel="noopener noreferrer"
+          locale={false} 
+          download
+        >
+          <b>{dictionary.bio.resume}</b>
+        </Link>
       </div>
     </main>
   )
