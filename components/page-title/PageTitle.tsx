73628de2d5c188282074b02101b10d0870b619b3
Felipe Miotto <felipe_miotto@hotmail.com>
2023-12-14 17:19:22 -0300

    style: page title font

--- components/page-title/PageTitle.tsx
+++ components/page-title/PageTitle.tsx
@@ -7,7 +7,7 @@ type PageTitleProps = {
 function PageTitle({ title }: PageTitleProps) {
   return (
     <div className="text-5xl font-[bold] pb-[30px]">
-      <h1>{title}</h1>
+      <h1 className='text-2xl md:text-3xl lg:text-4xl'>{title}</h1>
       <hr className="rounded-[3px] border-[3px] border-solid"/>
     </div>
   )
